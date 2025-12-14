ba6070a02b9de8a4f1752dd7201086646ebbd23c
ark-ku07 <kuznetsov2007.kuznetsov@ya.ru>
2025-12-14 21:48:53 +0300

    Update cafe.h

--- cafe.h
+++ cafe.h
@@ -1,20 +1,28 @@
 #ifndef CAFE_H
 #define CAFE_H
+#define FILE_NAME "menu.txt"
 
 typedef struct Item {
     int id;
     char name[50];
     float price;
+    char category[30];
     struct Item* next;
 } Item;
 
-extern Item* head;
+typedef struct Menu {
+    Item* head;
+    int next_id;
+} Menu;
 
-void init();
-void show();
-void add();
-void find();
-void del();
-void save();
+void init_menu(Menu* menu);
+void show(Menu* menu);
+void add(Menu* menu);
+void edit(Menu* menu);
+void find_by_price(Menu* menu);
+void find_by_category(Menu* menu);
+void del(Menu* menu);
+void save(Menu* menu);
+void load(Menu* menu);
 
 #endif
